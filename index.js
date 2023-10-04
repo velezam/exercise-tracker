ed38d24466ebd749a58212dd8ace25921d9db1ee
Amadeo Velez <amadeo.velez@gmail.com>
2023-10-04 13:28:22 -0400

    added mongoose

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+// connect database
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
